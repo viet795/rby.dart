544d435113c5b0d57fd18e8c73e0ee725b501bc7
viet795 <93976915+viet795@users.noreply.github.com>
2023-02-16 15:19:43 +0700

    Update rby_list_tile.dart

--- lib/widgets/rby_list_tile.dart
+++ lib/widgets/rby_list_tile.dart
@@ -51,7 +51,7 @@ class RbyListTile extends StatelessWidget {
       borderRadius: borderRadius,
       child: InkWell(
         onTap: enabled ? onTap : null,
-        onLongPress: enabled ? onLongPress : null
+        onLongPress: enabled ? onLongPress : null,
         borderRadius: borderRadius,
         child: IconTheme(
           data: theme.iconTheme.copyWith(
